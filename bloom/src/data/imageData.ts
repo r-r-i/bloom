ee5f7837b946aa6ce6e9433ad35ca3bd79f3c8bc
Ruben Iacono <riaconoo@icloud.com>
2024-10-10 15:25:39 +1100

    add images

--- bloom/src/data/imageData.ts
+++ bloom/src/data/imageData.ts
@@ -1,5 +1,119 @@
 export const imageData = [
     {
+        id: 477,
+        url: '/assets/images/477.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 478,
+        url: '/assets/images/478.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 479,
+        url: '/assets/images/479.png',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 480,
+        url: '/assets/images/480.jpg',
+        alt: 'missing image',
+        description: 'FKA twigs paying homage to the Excel texting in Nelly and Kelly Rowland`s Dilemma video from 2002'
+    },
+    {
+        id: 481,
+        url: '/assets/images/481.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 482,
+        url: '/assets/images/482.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 483,
+        url: '/assets/images/483.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 484,
+        url: '/assets/images/484.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 485,
+        url: '/assets/images/485.jpg',
+        alt: 'missing image',
+        description: 'Vagabond (1998 - 2015)'
+    },
+    {
+        id: 486,
+        url: '/assets/images/486.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 487,
+        url: '/assets/images/487.png',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 488,
+        url: '/assets/images/488.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 489,
+        url: '/assets/images/489.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 490,
+        url: '/assets/images/490.jpg',
+        alt: 'missing image',
+        description: 'Female Pentimento explores the tension between beauty and decay through her blend of photography and digital manipulation'
+    },
+    {
+        id: 491,
+        url: '/assets/images/491.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 492,
+        url: '/assets/images/492.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 493,
+        url: '/assets/images/493.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 494,
+        url: '/assets/images/494.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
+        id: 495,
+        url: '/assets/images/495.jpg',
+        alt: 'missing image',
+        description: ''
+    },
+    {
         id: 496,
         url: '/assets/images/496.jpg',
         alt: 'missing image',
